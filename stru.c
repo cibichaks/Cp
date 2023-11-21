f184969d7ec603ce2ee546b800f23417c3823069
sibichakss <sibisk1328327@gmail.com>
2023-11-22 00:01:37 +0530

    commited after done the learning of the structure

--- stru.c
+++ stru.c
@@ -49,6 +49,60 @@ int main(){
     printf("%s",p->brand);
 }
 */
+//passing structure to a funtion
+/*
+struct lap
+    {
+        char brand[25];
+        int Dsize;
+        int ram;
+    };
+void finder(struct lap);
 int main (){
-    
+ struct lap cibi={"dell",15,8};
+ finder(cibi);
+}
+void finder(struct lap cibi){
+    printf("%d",cibi.Dsize);
+}
+*/
+//passing structure pointer to a function
+/*
+struct lap
+    {
+        char brand[25];
+        int Dsize;
+        int ram;
+    };
+void ptrfun(struct lap *cib);
+void main(){
+    struct lap cibi={"dell",15,8};
+    struct lap *p;
+    p=&cibi;
+    ptrfun(p);
+}
+void ptrfun(struct lap *cib){
+    printf("%d",cib->ram);
+}
+*/
+///////typedef excercise 
+typedef struct {
+    char brand[25];
+    int Dsize;
+    int ram;
+}laptop;
+
+void typeex(laptop *det);
+int main(){
+    laptop chaks={"dell",15,8};
+    struct laptop *p;
+    p=&chaks;
+    typeex(p);
+}
+void typeex(laptop *det){
+    if (det->Dsize == det->ram){
+        printf ("dummy bhava");
+    }
+    else
+    printf("massehh");
 }
